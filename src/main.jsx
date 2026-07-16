510ee61ea83f85ead218ea2fe160f454ae2b25fb
Lucky783594 <pagallucky692@gmail.com>
2026-07-16 17:27:24 +0530

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { PlayerProvider } from "./context/PlayerContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter 
+      future={{ 
+        v7_startTransition: true, 
+        v7_relativeSplatPath: true 
+      }}
+    >
       <AuthProvider>
         <PlayerProvider>
           <App />
